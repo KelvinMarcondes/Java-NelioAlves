6221237d19a2d6b4b9fbf1c2984ba096c1f2e82a
Kelvin-Marcondes <kelvin.marcondes@ibm.com>
2022-09-15 09:13:12 -0300

    add section 14

--- Seção_13/src/entities/Client.java
+++ Seção_13/src/entities/Client.java
@@ -46,7 +46,7 @@ public class Client {
 
 	@Override
 	public String toString() {
-		return name + " (" +sdf.format(birthDate) + " ) - " + email;
+		return name + " (" +sdf.format(birthDate) + ") - " + email;
 	}
 	
 	
