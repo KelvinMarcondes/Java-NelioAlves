048f23e298c087ea4a94cccc891c2b9f1bad6cf4
Kelvin Marcondes <kelvin.marcondes@ibm.com>
2022-10-20 17:31:06 -0300

    att Contas

---
+++ Section_14_Heranca/src/entities/Conta.java
@@ -0,0 +1,48 @@
+package entities;
+
+public class Conta {
+	
+	private Integer numero;
+	private String proprietario;
+	private Double saldo;
+	
+	public Conta() {
+	}
+
+	public Conta(Integer numero, String proprietario, Double saldo) {
+		super();
+		this.numero = numero;
+		this.proprietario = proprietario;
+		this.saldo = saldo;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getProprietario() {
+		return proprietario;
+	}
+
+	public void setProprietario(String proprietario) {
+		this.proprietario = proprietario;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public void saque(double valor) {
+		saldo -= valor;
+	}
+	
+	public void deposito(double valor) {
+		saldo += valor;
+	}
+	
+
+}
---
+++ Section_14_Heranca/src/entities/ContaBusiness.java
@@ -0,0 +1,9 @@
+package entities;
+
+public class ContaBusiness extends Conta{
+	
+	private Double LimiteEmprestimo;
+	
+	
+
+}
