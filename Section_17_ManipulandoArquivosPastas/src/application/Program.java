771bc8b22b0f7109e70c9a090c8fe34d5e6b62db
Kelvin <kelvin.marconde@ibm.com>
2022-11-01 21:04:34 -0300

    "Atualizando e testando novos plugins"

--- Section_17_ManipulandoArquivosPastas/src/application/Program.java
+++ Section_17_ManipulandoArquivosPastas/src/application/Program.java
@@ -1,9 +1,28 @@
 package application;
 
+import entities.Product;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        List<Product> list = new ArrayList<>();
+        System.out.println("Enter file path: ");
+        String sourceFileStr = scanner.nextLine();
+
+        File souFile = new File(sourceFileStr);
+
+
+        scanner.close();
 
 
     }
