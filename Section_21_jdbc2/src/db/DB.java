9fa3f5c72ff910623b5e67452e6b15cebb54980a
Kelvin Marcondes <kelvindesouza@hotmail.com>
2022-12-15 21:42:08 -0300

    Recuperando dados do DB e tratando exceções de Statement e ResultSet com try catch

--- Section_21_jdbc2/src/db/DB.java
+++ Section_21_jdbc2/src/db/DB.java
@@ -4,11 +4,13 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
-	
+
 	private static Connection conn = null;
 	
 	public static Connection getConnection() {
@@ -24,7 +26,27 @@ public class DB {
 		}
 		return conn;
 	}
+
 	
+	public static void closeStatement(Statement st) {
+		if(st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
 	
 	public static void closeConnection() {
 		if(conn != null) {
@@ -47,4 +69,5 @@ public class DB {
 			throw new DbException(e.getMessage());
 		}
 	}
+
 }
