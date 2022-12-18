56ea7312fff010b8825a7213bbe22bcad3e6c935
Kelvin Marcondes <kelvindesouza@hotmail.com>
2022-12-18 03:19:26 -0300

    implementado o método insert

--- Section_21_jdbc7_DAO/src/application/Program.java
+++ Section_21_jdbc7_DAO/src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -13,16 +14,15 @@ public class Program {
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-
-		System.out.println("==== TEST 1:  seller findById ====");
 		
+		
+		System.out.println("==== TEST 1:  seller findById ====");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
 		
 		
 		System.out.println("\n==== TEST 2:  seller findByDepartment ====");
-		
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller seller1 : list) {
@@ -32,12 +32,17 @@ public class Program {
 		
 		
 		System.out.println("\n==== TEST 3:  seller findAll ====");
-		
 		list = sellerDao.findAll();
 		for (Seller seller1 : list) {
 			System.out.println(seller1);
-
 		}
+			
+		
+		System.out.println("\n==== TEST 4:  seller insert ====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inseted. New id = " + newSeller.getId());
+
 	}
 
 }
