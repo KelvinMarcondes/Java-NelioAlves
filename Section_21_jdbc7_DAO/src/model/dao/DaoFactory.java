0cd5abf4fa1aa922d7974d430152a2eba11e953d
Kelvin Marcondes <kelvindesouza@hotmail.com>
2022-12-18 23:58:09 -0300

    implementado métodos insert, update, findByID, findAll e delete do DepartmentDaoJDBC

--- Section_21_jdbc7_DAO/src/model/dao/DaoFactory.java
+++ Section_21_jdbc7_DAO/src/model/dao/DaoFactory.java
@@ -2,6 +2,7 @@ package model.dao;
 
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
@@ -10,6 +11,8 @@ public class DaoFactory {
 		return new SellerDaoJDBC(DB.getConnection());
 	}
 	
-
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
 
 }
